a40d67e07ab7ec23fb1c723b49ef920ded572407
jhudielden <138587752+jhudielden@users.noreply.github.com>
2024-12-03 12:31:41 +0000

    update

--- Assets/Scripts/FollowCharacter.cs
+++ Assets/Scripts/FollowCharacter.cs
@@ -7,12 +7,13 @@ public class FollowCharacter : MonoBehaviour
     // Script from https://youtu.be/FXqwunFQuao?si=MSj36lxP8ikZEhj_
     public float FollowSpeed = 2f;
     public float yOffset = 1f;
+    public float xOffset = 1f;
     public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x +xOffset,target.position.y + yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed*Time.deltaTime);
     }
 }
