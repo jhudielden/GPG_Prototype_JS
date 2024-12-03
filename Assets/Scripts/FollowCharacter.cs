1784519651fa351964ed83cb7235ddbb285f328f
jhudielden <138587752+jhudielden@users.noreply.github.com>
2024-12-03 13:39:41 +0000

    update

--- Assets/Scripts/FollowCharacter.cs
+++ Assets/Scripts/FollowCharacter.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class FollowCharacter : MonoBehaviour
 {
-    // Script from https://youtu.be/FXqwunFQuao?si=MSj36lxP8ikZEhj_
+    // Movement Script https://youtu.be/FXqwunFQuao?si=MSj36lxP8ikZEhj_
+    // Clamping Script https://youtu.be/Fqht4gyqFbo?si=vquIaBIUKIUl3aTG
     public float FollowSpeed = 2f;
-    public float yOffset = 1f;
-    public float xOffset = 1f;
+   
     public Transform target;
+    public Vector3 minValues, maxValues;
+    public Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x +xOffset,target.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed*Time.deltaTime);
+        Vector3 targetPosition = target.position + offset;
+        Vector3 boundPosition = new Vector3(
+              Mathf.Clamp(targetPosition.x, minValues.x, maxValues.x),
+              Mathf.Clamp(targetPosition.y, minValues.y, maxValues.y),
+              Mathf.Clamp(targetPosition.z, minValues.z, maxValues.z)
+              );
+        
+        transform.position = Vector3.Slerp(transform.position, boundPosition, FollowSpeed * Time.deltaTime);
+       
     }
 }
